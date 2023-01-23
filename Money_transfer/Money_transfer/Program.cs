0af0c0ee07fd108bec228648bdf51882d5289782
apetunenkovaa <apetunenkovaa@mail.ru>
2023-01-23 08:28:46 +0300

    first

---
+++ Money_transfer/Money_transfer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Money_transfer
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<money> arr = new List<money>();
+            Read.read(ref arr);
+            double transfer;
+            foreach (var item in arr)
+            {
+                if (!(item.rub == 0 && item.kop == 0))
+                {
+                    transfer = item.rub + (item.kop / 100) * 0.017;
+                    item.dol = transfer;
+
+                    transfer = item.rub + (item.kop / 100) * 0.018;
+                    item.euro = transfer;
+
+                    transfer = item.rub + (item.kop / 100) * 0.016;
+                    item.pound = transfer;
+
+                    transfer = item.rub + (item.kop / 100) * 7.10;
+                    item.dram = transfer;
+
+                    transfer = item.rub + (item.kop / 100) * 2.48;
+                    item.yen = transfer;
+                }
+                else
+                {
+                    if()
+                }
+                Console.WriteLine($"{item.rub} {item.kop} {item.dol} {item.euro} {item.pound} {item.dram} {item.yen}");
+            }
+        }
+
+        public struct money
+        {
+            public double rub;
+            public double kop;
+            public double dol;
+            public double euro;
+            public double pound;
+            public double dram;
+            public double yen;
+        }
+    }
+}
---
+++ Money_transfer/Money_transfer/Read.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Money_transfer
+{
+    class Read
+    {
+        public static void read(ref List<program.money> m)
+        {
+            string path = Environment.CurrentDirectory;
+            path = path.Replace("bin\\Debug\\net6.0", "dataMoney.csv");
+            string[] l = File.ReadAllLines(path, Encoding.Default);
+            foreach (var line in l)
+            {
+                string[] s = line.Split(';');
+                program.money a = new program.money();
+
+                check(s[0], out a.rub);
+                check(s[1], out a.kop);
+                check(s[2], out a.dol);
+                check(s[3], out a.euro);
+                check(s[4], out a.rup);
+                check(s[5], out a.kron);
+                check(s[6], out a.yuan);
+
+                m.Add(a);
+            }
+        }
+
+        static void check(string s, out double a)
+        {
+            if(Regex.IsMatch(s, @"^[0-9]+$"))
+            {
+                if (!String.IsNullOrEmpty(s))
+                {
+                    a = Convert.ToInt32(s);
+                }
+                else
+                {
+                    a = 0;
+                }
+            }
+            else
+            {
+                a = 0;
+            }
+        }
+    }
+}
